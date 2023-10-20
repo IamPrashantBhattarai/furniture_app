ba41c223378c553f3b5844f6f983683ff11c4c23
Prashant <prashantbhattarainepal@gmail.com>
2023-10-20 13:40:48 +0545

    Adding the constructor and factory constructor of Product

---
+++ lib/models/Product.dart
@@ -0,0 +1,28 @@
+class Product {
+  final int? price;
+  final String? id, title, category, image, subTitle, description;
+
+  Product({
+    this.id,
+    this.price,
+    this.title,
+    this.category,
+    this.image,
+    this.subTitle,
+    this.description,
+  });
+  //factory constructor from Json that is used while working with the APIs
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json["id"],
+      title: json["title"],
+      image: json["image"],
+      price: json["price"],
+      category: json["category"],
+      subTitle: json["subTitle"],
+      description: json["description"],
+    );
+  }
+}
+
+//
--- lib/size_config.dart
+++ lib/size_config.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class SizeConfig {
   static MediaQueryData _mediaQueryData;
-  static double screenWidth;
-  static double screenHeight;
-  static double defaultSize;
-  static Orientation orientation;
+  static double? screenWidth;
+  static double? screenHeight;
+  static double? defaultSize;
+  static Orientation? orientation;
 
   void init(BuildContext context) {
     _mediaQueryData = MediaQuery.of(context);
@@ -14,7 +14,7 @@ class SizeConfig {
     orientation = _mediaQueryData.orientation;
 
     defaultSize = orientation == Orientation.landscape
-        ? screenHeight * 0.024
-        : screenWidth * 0.024;
+        ? screenHeight! * 0.024
+        : screenWidth! * 0.024;
   }
 }
