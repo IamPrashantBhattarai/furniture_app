b2ecbb06903deaa77caebbbeca2b2f5e133067c5
Prashant <prashantbhattarainepal@gmail.com>
2023-10-23 16:49:31 +0545

    Body being developed

---
+++ lib/screens/components/body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:furniture_ui/size_config.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    double? defaultSize = SizeConfig.defaultSize;
+    return SingleChildScrollView(
+      child: Column(
+        children: [],
+      ),
+    );
+  }
+}
