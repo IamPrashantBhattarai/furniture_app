25c410de5d8abc5dfc4b268deb9bdf0acbeaf316
Prashant <prashantbhattarainepal@gmail.com>
2023-10-23 22:48:07 +0545

    Adding a line in body

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -4,10 +4,36 @@ import 'package:furniture_ui/size_config.dart';
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double? defaultSize = SizeConfig.defaultSize;
     return SingleChildScrollView(
       child: Column(
-        children: [],
+        children: <Widget>[
+          TitleText(
+            title: "Browse by categories",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleText extends StatelessWidget {
+  const TitleText({
+    super.key,
+    required this.title,
+    this.defaultSize,
+  });
+
+  final double? defaultSize;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    double? defaultSize = SizeConfig.defaultSize;
+    return Text(
+      title,
+      style: TextStyle(
+        fontSize: defaultSize! * 1.6,
+        fontWeight: FontWeight.bold,
       ),
     );
   }
