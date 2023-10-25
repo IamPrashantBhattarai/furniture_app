dbbd6dbc0728a876fafad18285e2ebca89576bed
Prashant <prashantbhattarainepal@gmail.com>
2023-10-25 12:58:01 +0545

    Styling the box

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -24,10 +24,11 @@ class Body extends StatelessWidget {
                 alignment: Alignment.bottomCenter,
                 children: <Widget>[
                   Container(
-                    color: Colors.blueGrey,
+                    color: Colors.blueAccent,
                   ),
                   //As this is our custom shape so we should use ClipPath for round corners
                   ClipPath(
+                    clipper: CategoryCustomShape(),
                     child: AspectRatio(
                       aspectRatio: 1.025,
                       child: Container(
@@ -51,7 +52,17 @@ class CategoryCustomShape extends CustomClipper<Path> {
     Path path = Path();
     double height = size.height;
     double width = size.width;
-    int cornerSize = 30;
+    double cornerSize = 30;
+
+    path.lineTo(0, height - cornerSize);
+    path.quadraticBezierTo(0, height, cornerSize, height);
+    path.lineTo(width - cornerSize, height);
+    path.quadraticBezierTo(width, height, width, height - cornerSize);
+    path.lineTo(width, cornerSize);
+    path.quadraticBezierTo(width, 0, width - cornerSize, 0);
+    path.lineTo(cornerSize, cornerSize * 0.75);
+    path.quadraticBezierTo(0, cornerSize, 0, cornerSize * 2);
+    path.close();
 
     return path;
   }
