a770e032a147122d3dab3dba87c1e007f46ca765
Prashant <prashantbhattarainepal@gmail.com>
2023-10-25 15:39:36 +0545

    category card done

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:furniture_ui/components/title_text.dart';
 import 'package:furniture_ui/constants.dart';
+import 'package:furniture_ui/models/Categories.dart';
+import 'package:furniture_ui/screens/components/category_card.dart';
 import 'package:furniture_ui/size_config.dart';
 
 class Body extends StatelessWidget {
@@ -16,59 +18,11 @@ class Body extends StatelessWidget {
               title: "Browse by categories",
             ),
           ),
-          SizedBox(
-            width: defaultSize * 20.5, //205
-            child: AspectRatio(
-              aspectRatio: 0.83,
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: <Widget>[
-                  Container(
-                    color: Colors.blueAccent,
-                  ),
-                  //As this is our custom shape so we should use ClipPath for round corners
-                  ClipPath(
-                    clipper: CategoryCustomShape(),
-                    child: AspectRatio(
-                      aspectRatio: 1.025,
-                      child: Container(
-                        color: kSecondaryColor,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
+          CategoryCard(
+            category: category,
+          ),
         ],
       ),
     );
-  }
-}
-
-class CategoryCustomShape extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Path path = Path();
-    double height = size.height;
-    double width = size.width;
-    double cornerSize = 30;
-
-    path.lineTo(0, height - cornerSize);
-    path.quadraticBezierTo(0, height, cornerSize, height);
-    path.lineTo(width - cornerSize, height);
-    path.quadraticBezierTo(width, height, width, height - cornerSize);
-    path.lineTo(width, cornerSize);
-    path.quadraticBezierTo(width, 0, width - cornerSize, 0);
-    path.lineTo(cornerSize, cornerSize * 0.75);
-    path.quadraticBezierTo(0, cornerSize, 0, cornerSize * 2);
-    path.close();
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true;
   }
 }
