d56e6f0c7d47aa8e2dfda142e4636f820add4e85
Prashant <prashantbhattarainepal@gmail.com>
2023-10-31 14:12:52 +0545

    Styling being added

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:furniture_ui/components/title_text.dart';
+import 'package:furniture_ui/constants.dart';
+import 'package:furniture_ui/models/Product.dart';
 import 'package:furniture_ui/screens/components/categories.dart';
 import 'package:furniture_ui/services/fetch_Categories.dart';
 import 'package:furniture_ui/size_config.dart';
@@ -21,14 +24,43 @@ class Body extends StatelessWidget {
             ),
           ),
           FutureBuilder(
-              future: fetchCategories(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Categories(categories: snapshot.data!);
-                } else {
-                  return Center(child: Image.asset("assets/ripple.gif"));
-                }
-              })
+            future: fetchCategories(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Categories(categories: snapshot.data!);
+              } else {
+                return Center(child: Image.asset("assets/ripple.gif"));
+              }
+            },
+          ),
+          Divider(
+            height: 5,
+          ),
+          Padding(
+            padding: EdgeInsets.all(defaultSize * 2),
+            child: TitleText(title: "Recommends for You"),
+          ),
+          Container(
+            width: defaultSize * 14.5,
+            decoration: BoxDecoration(
+              color: kSecondaryColor,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: AspectRatio(
+              aspectRatio: 0.693,
+              child: Column(
+                children: <Widget>[
+                  AspectRatio(
+                    aspectRatio: 1,
+                    child: FadeInImage.assetNetwork(
+                      placeholder: "assets/spinner.gif",
+                      image: product.image,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
