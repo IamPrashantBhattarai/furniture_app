fde4356fc265ad653819eb3e4d464448524d6a9a
Prashant <prashantbhattarainepal@gmail.com>
2023-11-01 15:48:06 +0545

    Product card onTap added

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -42,6 +42,7 @@ class Body extends StatelessWidget {
           ),
           ProductCard(
             product: product,
+            press: () {},
           )
         ],
       ),
@@ -53,39 +54,44 @@ class ProductCard extends StatelessWidget {
   const ProductCard({
     super.key,
     required this.product,
+    required this.press,
   });
 
   final Product product;
+  final Function press;
 
   @override
   Widget build(BuildContext context) {
     double? defaultSize = SizeConfig.defaultSize;
-    return Container(
-      width: defaultSize! * 14.5,
-      decoration: BoxDecoration(
-        color: kSecondaryColor,
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: AspectRatio(
-        aspectRatio: 0.693,
-        child: Column(
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 1,
-              child: FadeInImage.assetNetwork(
-                placeholder: "assets/spinner.gif",
-                image: product.image,
-                fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        width: defaultSize! * 14.5,
+        decoration: BoxDecoration(
+          color: kSecondaryColor,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: AspectRatio(
+          aspectRatio: 0.693,
+          child: Column(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 1,
+                child: FadeInImage.assetNetwork(
+                  placeholder: "assets/spinner.gif",
+                  image: product.image,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: defaultSize),
-              child: TitleText(title: product.title),
-            ),
-            SizedBox(height: defaultSize / 2),
-            Text("\$${product.price}"),
-            Spacer(),
-          ],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: defaultSize),
+                child: TitleText(title: product.title),
+              ),
+              SizedBox(height: defaultSize / 2),
+              Text("\$${product.price}"),
+              Spacer(),
+            ],
+          ),
         ),
       ),
     );
