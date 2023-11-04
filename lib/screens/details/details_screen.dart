bfbf8e13d032178837f9898b3287d91b0c2778db
Prashant <prashantbhattarainepal@gmail.com>
2023-11-04 20:55:32 +0545

    Next button added

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:furniture_ui/constants.dart';
 import 'package:furniture_ui/models/Product.dart';
+import 'package:furniture_ui/size_config.dart';
 
 class DetailsScreen extends StatelessWidget {
   final Product product;
@@ -8,13 +10,22 @@ class DetailsScreen extends StatelessWidget {
   const DetailsScreen({Key? key, required this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
       backgroundColor: kSecondaryColor,
       appBar: AppBar(
         leading: IconButton(
           icon: SvgPicture.asset("assets/icons/arrow-long-left.svg"),
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
+        actions: <Widgets>[
+          IconButton(
+            onPressed: onPressed(){},
+            icon: SvgPicture.asset("assets/icons/bag.svg"),
+          )
+        ],
       ),
     );
   }
