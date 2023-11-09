3721c9f8d74d5aa280be2bce0bac49a52e2d5c7a
Prashant <prashantbhattarainepal@gmail.com>
2023-11-09 14:15:41 +0545

    product description done

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:furniture_ui/constants.dart";
 import "package:furniture_ui/models/Product.dart";
 import "package:furniture_ui/screens/details/components/product_info.dart";
 import 'package:furniture_ui/size_config.dart';
@@ -13,21 +14,45 @@ class Body extends StatelessWidget {
     double? defaultSize = SizeConfig.defaultSize;
     return SingleChildScrollView(
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           ProductInfo(product: product),
           Container(
-            height: 500,
+            padding: EdgeInsets.only(
+              top: defaultSize! * 9,
+              left: defaultSize * 2,
+              right: defaultSize * 2,
+            ),
+            // height: 500,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(defaultSize! * 1.2),
+                topLeft: Radius.circular(defaultSize * 1.2),
                 topRight: Radius.circular(defaultSize * 1.2),
               ),
             ),
             child: Column(
-              children: <Widget>[],
+              children: <Widget>[
+                Text(
+                  product.subTitle.toString(),
+                  style: TextStyle(
+                    fontSize: defaultSize * 1.8,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: defaultSize * 3,
+                ),
+                Text(
+                  product.description.toString(),
+                  style: TextStyle(
+                    color: kTextColor.withOpacity(0.7),
+                    height: 1.5,
+                  ),
+                ),
+              ],
             ),
-          )
+          ),
         ],
       ),
     );
