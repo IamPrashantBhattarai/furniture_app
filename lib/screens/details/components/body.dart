2bd613a189e9fda200aae4cc36533e2150b5ab22
Prashant <prashantbhattarainepal@gmail.com>
2023-11-11 21:57:35 +0545

    Adding image to the details screen

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:furniture_ui/models/Product.dart";
 import "package:furniture_ui/screens/details/components/product_description.dart";
 import "package:furniture_ui/screens/details/components/product_info.dart";
+import "package:furniture_ui/size_config.dart";
 
 class Body extends StatelessWidget {
   final Product product;
@@ -10,16 +11,36 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double? defaultSize = SizeConfig.defaultSize;
     return SingleChildScrollView(
-      child: Stack(
-        // crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          ProductInfo(product: product),
-          ProductDescription(
-            product: product,
-            press: () {},
-          )
-        ],
+      child: SizedBox(
+        width: double.infinity,
+        height: SizeConfig.screenHeight! - AppBar().preferredSize.height,
+        child: Stack(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ProductInfo(product: product),
+            Positioned(
+              top: defaultSize! * 37.5,
+              left: 0,
+              right: 0,
+              child: ProductDescription(
+                product: product,
+                press: () {},
+              ),
+            ),
+            Positioned(
+              top: defaultSize * 9.5,
+              right: -defaultSize * 7.5,
+              child: Image.network(
+                product.image.toString(),
+                fit: BoxFit.cover,
+                height: defaultSize * 37.8,
+                width: defaultSize * 36.4,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
